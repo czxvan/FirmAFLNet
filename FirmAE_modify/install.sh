671607dfae21069911767979d421f8b5f9edb225
czxvan <2889259641@qq.com>
2024-07-14 20:58:36 +0800

    fix firmae schema ignore wrong

--- FirmAE_modify/install.sh
+++ FirmAE_modify/install.sh
@@ -5,6 +5,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt install -y postgresql
 sudo /etc/init.d/postgresql restart
 sudo -u postgres bash -c "psql -c \"CREATE USER firmadyne WITH PASSWORD 'firmadyne';\""
 sudo -u postgres createdb -O firmadyne firmware
+sudo -u postgres psql -d firmware < database/schema
 sudo -u postgres psql -d firmware < ../FirmAE_modify/data
 echo "listen_addresses = '172.17.0.1,127.0.0.1,localhost'" | sudo -u postgres tee --append /etc/postgresql/*/main/postgresql.conf
 echo "host all all 172.17.0.1/24 trust" | sudo -u postgres tee --append /etc/postgresql/*/main/pg_hba.conf
