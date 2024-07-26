3799a984b5c3aa52b6473eb983c3f1747bdea48a
czxvan <2889259641@qq.com>
2024-07-26 21:41:06 +0800

    update configs

--- FirmAFLNet_config/3473/scripts/restart_target.sh
+++ FirmAFLNet_config/3473/scripts/restart_target.sh
@@ -1,8 +1,8 @@
 TARGET_NAME=httpd
 # IP=$(cat ip)
-IP=192.168.0.1
+IP=192.168.1.1
 CMD="pids=\`ps | grep httpd | grep -v grep | sed 's/^ *//' | cut -d ' ' -f 1\` && for pid in \$pids; do kill -9 \$pid; done; \
-    httpd -f /var/run/httpd.conf"
+    httpd -S -E /usr/sbin/ca.pem /usr/sbin/httpsd.pem"
 
 curl $IP:4817/execute \
     -d "{\"cmd\": \"$CMD\"}" \
--- FirmAFLNet_config/3473/scripts/test_alive.sh
+++ FirmAFLNet_config/3473/scripts/test_alive.sh
@@ -1,6 +1,6 @@
 TARGET_NAME=httpd
 # IP=$(cat ip)
-IP=192.168.0.1
+IP=192.168.1.1
 
 curl $IP --max-time 2 \
     >& /dev/null
