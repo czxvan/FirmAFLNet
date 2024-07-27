109f37fd0e02e552d425984f6643bd00f0b034ca
czxvan <2889259641@qq.com>
2024-07-28 00:03:06 +0800

    update setup scripts

--- FAN_setup.py
+++ FAN_setup.py
@@ -3,7 +3,7 @@ import os
 
 firm_id = sys.argv[1]
 
-run_fan_src = "FirmAE/scratch/%s/run_fan.sh" % firm_id
+run_src = "FirmAE/scratch/%s/run.sh" % firm_id
 scripts_src = "FirmAFLNet_config/%s/scripts" % firm_id
 keywords_src = "FirmAFLNet_config/%s/keywords" % firm_id
 
@@ -12,9 +12,9 @@ seed_src = "FirmAFLNet_config/%s/seed" %(firm_id)
 dst_input = "image_%s/inputs/" %firm_id
 
 cmd = []
-cmd.append("cp %s %s" %(run_fan_src, dst))
-cmd.append("cp %s %s" %(keywords_src, dst)) 
 cmd.append("mkdir -p image_%s/inputs" % firm_id)
+cmd.append("cp %s %s" %(run_src, dst))
+cmd.append("cp %s %s" %(keywords_src, dst)) 
 cmd.append("cp %s %s" %(seed_src, dst_input)) 
 
 for i in range(0, len(cmd)):
--- generate_run_fan.py
+++ generate_run_fan.py
@@ -1,13 +1,32 @@
 import os
 import sys
 
+
+exit_function_str = \
+"""delete_device() {
+	echo "Bringing down TAP device..."
+	sudo ip link set ${TAPDEV_0} down
+
+
+	echo "Deleting TAP device ${TAPDEV_0}..."
+	sudo tunctl -d ${TAPDEV_0}
+	echo "Done!"
+}
+
+trap delete_device EXIT
+
+"""
+
 def generate_run_full(image_id):
 	script_src = "FirmAE/scratch/%s/run.sh" %image_id
-	script_dst = "FirmAE/scratch/%s/run_fan.sh" %image_id
+	script_dst = "image_%s/run_fan.sh" %image_id
 	file_src = open(script_src)
 	file_dst = open(script_dst, "w+")
 	for line in file_src.readlines():
-		if "Error:" in line:
+		if "TAPDEV_0=" in line:
+			file_dst.write(exit_function_str)
+			file_dst.write(line)
+		elif "Error:" in line or line in exit_function_str:
 			continue
 		elif "exit" in line:
 			file_dst.write("    source ../FirmAE/firmae.config\n")
@@ -18,15 +37,20 @@ def generate_run_full(image_id):
 		elif "Starting emulation of firmware..." in line:
 			file_dst.write("IP=$(cat ${WORK_DIR}/ip)\n")
 			file_dst.write(line)
-			file_dst.write("../afl-fuzz -i ${FUZZ_DIR}/inputs -o ${FUZZ_DIR}/outputs -P HTTP -N ${IP} -m none -QQ\\\n")
+			file_dst.write("../afl-fuzz -i ${FUZZ_DIR}/inputs -o ${FUZZ_DIR}/outputs -P HTTP -N tcp://${IP}/80 -m none -QQ\\\n")
 			file_dst.write("    -d -q 3 -s 3  -R -W 5 -w 50000 -t 50000  -x keywords \\\n")
 			file_dst.write("    ")
+		elif "QEMU_AUDIO_DRV=none" in line:
+			file_dst.write(line.replace("QEMU_AUDIO_DRV=none ", ""))
 		elif "-serial file:${WORK_DIR}/qemu.final.serial.log" in line:
 			file_dst.write(line.replace("WORK_DIR", "FUZZ_DIR"))
 		elif "-display none" in line:
 			file_dst.write(line)
 			file_dst.write("    -d plugin \\\n")
 			file_dst.write("    -plugin ${PLUGIN} \\\n")
+			file_dst.write("    -D qemu_log.txt \\\n")
+		elif "| true" in line:
+			file_dst.write(line.replace(" | true", ""))
 		else:
 			file_dst.write(line)
 	file_src.close()
