1fe80c4def8e6b6c0f3d8189ea31209eb284d99d
czxvan <2889259641@qq.com>
2024-07-29 00:59:36 +0800

    update hello-crows config

--- FirmAFLNet_config/bmc-https/runFuzz.sh
+++ FirmAFLNet_config/bmc-https/runFuzz.sh
@@ -1,11 +1,11 @@
-KERN=/home/czx/openbmc-workspace/openbmc/build/romulus/tmp/deploy/images/romulus
-QEMUSPY=/home/czx/qemu-workspace/qemu-spy
+KERN=/workspaces/firmaflnet-repro/FirmAFLNet/firmware/bmc
+QEMUSPY=/workspaces/firmaflnet-repro/FirmAFLNet/qemu_mode/SPY_qemu_8.2.91
 AFLSPY=$QEMUSPY/plugin_spy
-export AFL_FAST_CAL=1
+
 # gdbserver 127.0.0.1:1234 \
-# ../../afl-fuzz -i ./inputs -o ./outputs -P HTTPS -N tcp://127.0.0.1/18084 -m 4096M -QQ \
-#      -d -q 3 -s 3 -E -R -W 5 -w 50000 -t 50000 \
-#      -- \
+../../afl-fuzz -i ./inputs -o ./outputs -P HTTPS -N tcp://127.0.0.1/18084 -m 4096M -QQ \
+     -d -q 3 -s 3 -E -R -W 5 -w 50000 -t 50000 \
+     -- \
      $QEMUSPY/build/qemu-system-arm \
     -m 256 -machine romulus-bmc \
     -drive file=$KERN/obmc-phosphor-image-romulus.static.mtd,if=mtd,format=raw\
