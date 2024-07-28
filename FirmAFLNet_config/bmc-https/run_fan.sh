fa8550d0113ace1c7ac38394d32d7dda612a5a6b
czxvan <2889259641@qq.com>
2024-07-29 01:08:58 +0800

    add run_fan.sh for bmc-https

--- FirmAFLNet_config/bmc-https/run_fan.sh
+++ FirmAFLNet_config/bmc-https/run_fan.sh
@@ -3,7 +3,7 @@ QEMUSPY=/workspaces/firmaflnet-repro/FirmAFLNet/qemu_mode/SPY_qemu_8.2.91
 AFLSPY=$QEMUSPY/plugin_spy
 
 # gdbserver 127.0.0.1:1234 \
-../../afl-fuzz -i ./inputs -o ./outputs -P HTTPS -N tcp://127.0.0.1/18084 -m 4096M -QQ \
+../afl-fuzz -i ./inputs -o ./outputs -P HTTPS -N tcp://127.0.0.1/18084 -m 4096M -QQ \
      -d -q 3 -s 3 -E -R -W 5 -w 50000 -t 50000 \
      -- \
      $QEMUSPY/build/qemu-system-arm \
@@ -12,6 +12,6 @@ AFLSPY=$QEMUSPY/plugin_spy
     -net nic \
     -net user,hostfwd=:127.0.0.1:18080-:8080,hostfwd=:127.0.0.1:18084-:8084,hostfwd=:127.0.0.1:14817-:4817,hostname=qemu \
     -d plugin \
-    -plugin $AFLSPY/build/libaflspy.so \
+    -plugin $AFLSPY/build/libaflspy-arm.so \
     -D qemu_log.txt \
     -nographic \
