27d26a2e182c18bc1a9f830794eab1a8e17ac2ae
czxvan <2889259641@qq.com>
2024-07-29 15:27:29 +0800

    add bmc http config

---
+++ FirmAFLNet_config/bmc-http/scripts/test_alive.sh
@@ -0,0 +1,3 @@
+# Inform aflspy to set the context of the target process based on this request
+# --insecure is used to ignore the certificate verification
+curl 127.0.0.1:18080 --max-time 5 > /dev/null 2>&1
